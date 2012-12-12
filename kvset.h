1ec5319ad5bda3b58ee1c6fbe4920391d226f288
Kjell Winblad <kjellwinblad@gmail.com>
2012-12-12 09:13:05 +0100

    At least one gcc version complains about duplicate typdef declarations.

--- kvset.h
+++ kvset.h
@@ -1,22 +1,22 @@
 #ifndef __KVSET_H__
 #define __KVSET_H__
 
-typedef struct kv_set KVSet;
+struct kv_set;
 
 typedef struct kv_set_functions
 {   
-    void (*delete_table)(KVSet* kv_set,
+    void (*delete_table)(struct kv_set * kv_set,
                          void (*element_free_function)(void *context, void* element),
                          void * context);
-    void * (*put)(KVSet * set, void * key_value);
-    int (*put_new)(KVSet * set, void * key_value);
-    void * (*remove)(KVSet * set, void * key);
-    void * (*lookup)(KVSet * set, void * key);
-    int (*member)(KVSet * set, void * key);
-    void * (*first)(KVSet * set);
-    void * (*last)(KVSet * set);    
-    void * (*next)(KVSet * set, void * key);
-    void * (*previous)(KVSet * set, void * key);
+    void * (*put)(struct kv_set * kv_set, void * key_value);
+    int (*put_new)(struct kv_set * kv_set, void * key_value);
+    void * (*remove)(struct kv_set * kv_set, void * key);
+    void * (*lookup)(struct kv_set * kv_set, void * key);
+    int (*member)(struct kv_set * kv_set, void * key);
+    void * (*first)(struct kv_set * kv_set);
+    void * (*last)(struct kv_set * kv_set);    
+    void * (*next)(struct kv_set * kv_set, void * key);
+    void * (*previous)(struct kv_set * kv_set, void * key);
 } KVSetFunctions;
 
 
